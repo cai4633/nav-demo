50f33c81fc3361593c645f8568f3b63e394182d0
cai4633 <463353522@qq.com>
2020-03-18 01:40:35 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # 导航网页
 ## 主要功能
 1. 按下按键会打开对应的网页
-2. 每个按键可以通过点击<kbd>E<kbd>来自定义网址
+2. 每个按键可以通过点击<kbd>E</kbd>来自定义网址，并实时显示网站图标
 3. 通过localStorage来存储自定义网址
+5. 增加百度和谷歌搜索
+
+## 用到的知识点
+1. 利用box-shadow和:active伪类实现点击立体动画效果
+2. 使用display:inline-block搭配vertical-align:top避免baseline变化bug
+3. 利用/favicon.ico来获取网站icon
+4. 使用inline-block代替inline，避免inline padding-topw无效导致的bug
+5. user-select:none 消除点击元素文字变蓝的bug（浏览器默认行为）
+6. `img[src=''],img:not([src]){ opacity 0 } ` 去掉<kbd></kbd>加载失败样式
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// TODO: 怎样提高img加载速度？
 window.onload = function(){
     let hash = {
         'a': '163.com',
@@ -13,7 +14,7 @@ window.onload = function(){
         'k': 'kuaishou.com',
         'l': 'www.lagou.com/',
         'm': 'meituan.com',
-        'n': 'nba.com',
+        'n': 'www.nba.com',
         'o': 'v.ifeng.com',
         'p': 'sina.com',
         'q': 'qq.com',
@@ -24,7 +25,7 @@ window.onload = function(){
         'v': '163.com',
         'w': 'tudou.com',
         'x': 'youku.com',
-        'y': 'yahoo.com',
+        'y': 'www.yahoo.com',
         'z': 'zhihu.com'
     };
 
@@ -32,25 +33,47 @@ window.onload = function(){
         hash = JSON.parse(localStorage.getItem('hash'));
     }
     let layout = {
-        '0': ["q","w","e","r","t","y","u","i","o","p"],
-        '1': ["a","s","d","f","g","h","j","k","l"],
-        '2': ["z","x","c","v","b","n","m"],
+        '0': ["~","1","2","3","4","5","6","7","8","9","0","-","+",{'key': "delete",'className':'long right'}],
+        '1': [{'key': "tab",'className':'long left'},"q","w","e","r","t","y","u","i","o","p","{","}","|"],
+        '2': [{'key': "cap lock",'className':'enter left'},"a","s","d","f","g","h","j","k","l",":","'",{'key': "enter",'className':'enter right'}],
+        '3': [{'key': "shift",'className':'shift left'},"z","x","c","v","b","n","m","<",">","/",{'key': "shift",'className':'shift right'}],
     };
-
+    let keyLock = false;
     let layoutLength = Object.keys(layout).length;
     let root = document.getElementById('root');
+    let input = document.getElementById('keyword')
+    //当input 获得焦点时禁用keyboard
+    input.onfocus = function(){
+        keyLock = true
+    }
+    input.onblur = function(){
+        keyLock = false
+    }
+
     // 利用js创建element
     for (let i = 0; i < layoutLength; i++){
         let div = document.createElement('div');
         for(let j = 0; j<layout[i].length; j++){
             let kbd = document.createElement('kbd');
             let btn = document.createElement('button');
+            let img = document.createElement('img')
             btn.textContent = 'E';
-            btn.id = layout[i][j];      //元素的id属性可以用来存储字符串
-            kbd.textContent = layout[i][j];
+            if(Object.prototype.toString.call(layout[i][j]) === '[object Object]')
+            {          
+                let span = document.createElement('span')
+                span.textContent = layout[i][j]['key'];
+                btn.id = layout[i][j]['key'];      //元素的id属性可以用来存储字符串
+                kbd.appendChild(span)                
+                kbd.className = layout[i][j]['className'];
+            }else{
+                btn.id = layout[i][j];      //元素的id属性可以用来存储字符串
+                kbd.textContent = layout[i][j];
+            }
+            getFavicon(img,btn.id)
+            kbd.appendChild(img);
             kbd.appendChild(btn);
             
-    // 自定义网址
+    // 通过‘E’自定义网址
             btn.addEventListener('click',function(){
                 let url = prompt('请您输入一个你喜欢的网址！')
                 if(url.match(/\w+\.\w{2,6}/)){      //匹配网址格式
@@ -61,16 +84,30 @@ window.onload = function(){
                     //调用自身
                     confirm('网址格式错误，是否重新输入？') && arguments.callee();
                 }
+                getFavicon(this.previousSibling,this.id);
             })
     // 定义press事件
             document.addEventListener('keypress',function(evt){     //键盘输入事件最好绑定在document上
-                let url = 'https://' + hash[evt.key];
-                // window.location.href = url;
-                window.open(url,'_blank');
+                if(!keyLock){
+                    let url = 'https://' + hash[evt.key];
+                    // window.location.href = url;
+                    window.open(url,'_blank');                    
+                }
             })
             div.appendChild(kbd);
         }
-        root.appendChild(div);
-    }
 
+        root.appendChild(div);
+
+        //get icon from website
+        function getFavicon(image,key){
+            if(hash[key]){
+                image.src = "https://" + hash[key] + '/favicon.ico'    //get icon
+            }
+            image.onerror = function (){
+                image.src = 'https://s1.ax1x.com/2020/03/18/8d8UU0.png'
+            }
+        }
+    }
 }
+
